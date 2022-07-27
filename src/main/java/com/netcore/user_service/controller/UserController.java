3c5f85dfcc6a833ea47244ec8e2d8cd7f6249dfa
Shubham Nhalde <96485743+ShubhamNhalde@users.noreply.github.com>
2022-07-27 12:08:38 +0530

    new update

--- src/main/java/com/netcore/user_service/controller/UserController.java
+++ src/main/java/com/netcore/user_service/controller/UserController.java
@@ -2,6 +2,7 @@ package com.netcore.user_service.controller;
 
 import com.netcore.user_service.entity.User;
 import com.netcore.user_service.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/netcore/user_service/service/UserServiceImpl.java
+++ src/main/java/com/netcore/user_service/service/UserServiceImpl.java
@@ -1,20 +1,24 @@
 package com.netcore.user_service.service;
 
 import com.netcore.user_service.entity.User;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Slf4j
 public class UserServiceImpl implements UserService{
 
-    List<User> list = List.of(
-            new User(1311L, "Shubham Nhalde", "123456"),
-            new User(1312L, "Nikhil Patil", "654321"),
-            new User(1313L, "Sunil Patil", "888888")
+    List<User> list = Arrays.asList(
+            new User(1311L, "Shubham nhalde", "12345"),
+            new User(1312L, "Nikhil", "654321")
+
     );
     @Override
     public User getUser(Long id) {
+        log.info("Get Call For User Details");
         return this.list.stream().filter(user->user.getUserId().equals(id)).findAny().orElse(null);
     }
 }
